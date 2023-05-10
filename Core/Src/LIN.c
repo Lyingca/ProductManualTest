51ccaa50e8f273307f13ed764b4e2a7868199dc4
ChenJunjun <1415102261@qq.com>
2023-05-10 14:52:54 +0800

    将比较count值的代码放入for循环中，只要大于2就没在继续执行循环，提高响应速度

--- Core/Src/LIN.c
+++ Core/Src/LIN.c
@@ -241,10 +241,10 @@ uint8_t Check_Chip_Connection()
         {
             count++;
         }
-    }
-    if (count >= 2)
-    {
-        return 0;
+        if (count >= 2)
+        {
+            return 0;
+        }
     }
     return 1;
 }
